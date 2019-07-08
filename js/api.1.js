3feed6a3c3229da8fff577db39f7268fb597c53a
nurulik <nurulik1996@gmail.com>
2019-07-08 15:51:06 +0700

    commit pertama

---
+++ js/api.1.js
@@ -0,0 +1,122 @@
+var base_url = "https://readerapi.codepolitan.com/";
+
+// Blok kode yang akan di panggil jika fetch berhasil
+function status(response) {
+  if (response.status !== 200) {
+    console.log("Error : " + response.status);
+    // Method reject() akan membuat blok catch terpanggil
+    return Promise.reject(new Error(response.statusText));
+  } else {
+    // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
+    return Promise.resolve(response);
+  }
+}
+
+// Blok kode untuk memparsing json menjadi array JavaScript
+function json(response) {
+  return response.json();
+}
+
+// Blok kode untuk meng-handle kesalahan di blok catch
+function error(error) {
+  // Parameter error berasal dari Promise.reject()
+  console.log("Error : " + error);
+}
+
+// Blok kode untuk melakukan request data json
+function getAlbums() {
+  if ("caches" in window) {
+    caches.match(base_url + "articles").then(function(response) {
+      if (response) {
+        response.json().then(function(data) {
+          var articlesHTML = "";
+          data.result.forEach(function(album) {
+            articlesHTML += `
+                  <div class="card">
+                    <div class="card-content">
+                      <span class="card-title truncate">${album.id}</span>
+                      <p>${album.title}</p>
+                    </div>
+                  </div>
+                `;
+          });
+          // Sisipkan komponen card ke dalam elemen dengan id #content
+          document.getElementById("articles").innerHTML = articlesHTML;
+        });
+      }
+    });
+  }
+
+  fetch(base_url + "articles")
+    .then(status)
+    .then(json)
+    .then(function(data) {
+      // Objek/array JavaScript dari response.json() masuk lewat data.
+
+      // Menyusun komponen card artikel secara dinamis
+      var articlesHTML = "";
+      data.result.forEach(function(album) {
+        articlesHTML += `
+              <div class="card">
+                <div class="card-content">
+                  <span class="card-title truncate">${album.id}</span>
+                  <p>${album.title}</p>
+                </div>
+              </div>
+            `;
+      });
+      // Sisipkan komponen card ke dalam elemen dengan id #content
+      document.getElementById("articles").innerHTML = articlesHTML;
+    })
+    .catch(error);
+}
+
+function getAlbumById() {
+  // Ambil nilai query parameter (?id=)
+  var urlParams = new URLSearchParams(window.location.search);
+  var idParam = urlParams.get("id");
+
+  if ("caches" in window) {
+    caches.match(base_url + "album/" + idParam).then(function(response) {
+      if (response) {
+        response.json().then(function(data) {
+          var articleHTML = `
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img src="${data.result.cover}" />
+              </div>
+              <div class="card-content">
+                <span class="card-title">${data.result.post_title}</span>
+                ${snarkdown(data.result.post_content)}
+              </div>
+            </div>
+          `;
+          // Sisipkan komponen card ke dalam elemen dengan id #content
+          document.getElementById("body-content").innerHTML = articleHTML;
+        });
+      }
+    });
+  }
+
+  fetch(base_url + "album/" + idParam)
+    .then(status)
+    .then(json)
+    .then(function(data) {
+      // Objek JavaScript dari response.json() masuk lewat variabel data.
+      console.log(data);
+      // Menyusun komponen card artikel secara dinamis
+      var articleHTML = `
+          <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img src="${data.result.cover}" />
+            </div>
+            <div class="card-content">
+              <span class="card-title">${data.result.post_title}</span>
+              ${snarkdown(data.result.post_content)}
+            </div>
+          </div>
+        `;
+      // Sisipkan komponen card ke dalam elemen dengan id #content
+      document.getElementById("body-content").innerHTML = articleHTML;
+    });
+}
